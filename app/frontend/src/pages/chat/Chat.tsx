87f0d7bd76f26de57c039b62058b0a2c6032232c
Diego Lafuente <diego.lafuente@cognativinc.com>
2025-01-27 14:12:42 +0100

    Fixes to chat interface

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -367,7 +367,7 @@ const Chat = () => {
             <div className={styles.chatRoot} style={{ marginLeft: isHistoryPanelOpen ? "300px" : "0" }}>
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
-                        <div className={styles.chatEmptyState}>
+                        <div style={{ display: "none" }}>
                             <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
                             <h1 className={styles.chatEmptyStateTitle}>{t("chatEmptyStateTitle")}</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>{t("chatEmptyStateSubtitle")}</h2>
